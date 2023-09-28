6f7f3814aa4cbb359587405c2add22fec07af63e
AvailablA <manohar.shewale77@gmail.com>
2023-09-28 11:05:10 +0530

    The usestate hook for display name from user input add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+
+  const[name,setName]= useState("");
+
+  const[fName,fullName]= useState();
+  
+  const userIn= (input)=>
+  {
+    console.log(input.target.value);  //  inside inspect
+    setName(input.target.value)
+  }
+
+  const onSubmit= ()=>
+  {
+    fullName(name);
+  }
+  return(
+    <>
+      <div>
+        <h1>Hello  {fName} </h1>
+        <input type='text' placeholder='Enter your name'    // step-1
+                onChange={userIn}
+                value={name} />
+        <br/>                               
+        <button onClick={onSubmit}>SUBMIT</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
